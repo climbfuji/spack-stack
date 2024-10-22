43ab602ab05d06c91d143f3666bf76f741db8364
Dom Heinzeller <dom.heinzeller@icloud.com>
2024-10-22 09:31:42 -0600

    Update util/weekly_build scripts to handle multiple environments plus other updates

--- util/weekly_build/ShellSetup.sh
+++ util/weekly_build/ShellSetup.sh
@@ -11,16 +11,18 @@ if [ ${RUNDIR::1} != "/" ]; then
   exit 1
 fi
 
-PACKAGES_TO_TEST=${PACKAGES_TO_TEST:-"libpng libaec jasper scotch w3emc g2 g2c"}
+#PACKAGES_TO_TEST=${PACKAGES_TO_TEST:-"libpng libaec jasper scotch w3emc g2 g2c"}
 
 function alert_cmd {
   echo "Your run failed in $1. This is a placeholder alerting function. 'alert_cmd' should be defined for each system."
 }
 
 function spack_install_exe {
-  spack $* | tee -a log.install 2>&1
+  #spack $* | tee -a log.install 2>&1
+  spack $*
 }
 
+# Include platform-dependent configuration
 . $(dirname $0)/sites/${PLATFORM}.sh
 
 SOURCE_CACHE=${SOURCE_CACHE:-local-source}
